66633e84cdee9443c12df2e50ee224626427ba34
somminn <cowmin104@gmail.com>
2025-12-13 13:28:20 +0900

    [Feat] App.vue 추가

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="padding: 40px">
+    <h1>Frontend OK</h1>
+
+    <button @click="callBackend">
+      Call Backend
+    </button>
+
+    <p v-if="message">
+      Backend Response: {{ message }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import api from './api'
+
+const message = ref('')
+
+const callBackend = async () => {
+  const res = await api.get('/')
+  message.value = res.data
+}
+</script>
